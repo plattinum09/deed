4e2e13d01e940d44f75ab8f0fe0e9bc85d135459
Jrawat <plattinum_09@hotmail.com>
2017-08-30 17:59:39 +0700

    firstcommit

---
+++ src/routes.php
@@ -0,0 +1,44 @@
+<?php
+// Routes
+
+$app->get('/[{name}]', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+    // return $this->renderer->render($response, 'lanmark.phtml', $args);
+
+});
+
+$app->get('/lanmark/curl', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'lanmark.phtml', $args);
+});
+
+$app->get('/lanmark/appraise', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'Appraisebuilding.phtml', $args);
+});
+
+$app->get('/lanmark/appraise/detail', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'Appraisedetail.phtml', $args);
+});
+
+$app->get('/lanmark/deed', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'deed.phtml', $args);
+});
